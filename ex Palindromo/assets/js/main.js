475d0938ea228438a8d027085a1bc47534b422dc
fraberret <f.berretta02@gmail.com>
2024-02-27 19:29:45 +0100

    add final style ("Palindroma" exercise)

--- ex Palindromo/assets/js/main.js
+++ ex Palindromo/assets/js/main.js
@@ -1,25 +1,39 @@
 console.log('it works');
 
 
-//inserisco tutto dentro una funziona
 function palindromeCheck(userWord) {
-    
+
     //la reverso tramite reverse string e la salvo in una variabile
-    let reversedWord= userWord.split("").reverse().join("");
-    
+    let reversedWord = userWord.split("").reverse().join("");
+
+    document.getElementById('results').innerHTML=`Scritta al contrario è: ${reversedWord}`
+
     //SE parola dell utente è uguale a parola invertita stampo Palindroma
-    if (reversedWord==userWord) {
+    if (reversedWord == userWord) {
         alert('La tua parola è Palindroma')
-        
+
     }
     //Altrimenti stampo non palindroma
-    else{
+    else {
         alert('La tua parola non è Palindroma')
     }
-    
+
     return reversedWord
-    
+
 }
 
+
+
+
+document.getElementById('send_button').addEventListener('click', function (e) {
+    e.preventDefault
+    let inputElement = document.getElementById('user_word').value
+    console.log(inputElement);
+    console.log(palindromeCheck(inputElement));
+    
+})
+
+//inserisco tutto dentro una funziona
+
 // chiedo all utente una parola e la salvo dentro una variabile
-let userWord=palindromeCheck(prompt('Inserisci qui la tua parola'))
+
