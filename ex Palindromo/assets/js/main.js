53b2f140c8efb5e1726935046b7fb218790a78a1
fraberret <f.berretta02@gmail.com>
2024-02-27 16:07:07 +0100

    finish exercise ("Palindroma" exercise)

--- ex Palindromo/assets/js/main.js
+++ ex Palindromo/assets/js/main.js
@@ -1,1 +1,25 @@
 console.log('it works');
+
+
+//inserisco tutto dentro una funziona
+function palindromeCheck(userWord) {
+    
+    //la reverso tramite reverse string e la salvo in una variabile
+    let reversedWord= userWord.split("").reverse().join("");
+    
+    //SE parola dell utente è uguale a parola invertita stampo Palindroma
+    if (reversedWord==userWord) {
+        alert('La tua parola è Palindroma')
+        
+    }
+    //Altrimenti stampo non palindroma
+    else{
+        alert('La tua parola non è Palindroma')
+    }
+    
+    return reversedWord
+    
+}
+
+// chiedo all utente una parola e la salvo dentro una variabile
+let userWord=palindromeCheck(prompt('Inserisci qui la tua parola'))
